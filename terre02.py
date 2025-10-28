e9b7cbfa78985e228bb76231236d9f7c66e944be
Ayoub AMMOUCHE <ayoubammouche1@gmail.com>
2025-10-28 12:13:57 +0100

    epreuve 2 fini

---
+++ terre02.py
@@ -0,0 +1,10 @@
+import sys
+
+# Récupère les arguments à partir du troisième 
+mot = sys.argv[1:]
+
+# Boucle sur chaque élément de la liste
+
+for i in range(len(mot)):
+    coupage = mot[i]
+    print(coupage)
